4aa59db97a2e2c447eae4ff386306e7a12b55df4
Misha Sugakov <msugakov@redhat.com>
2025-11-16 11:37:45 +0100

    Update backup path

--- bin/backup-dropbox.sh
+++ bin/backup-dropbox.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-exec borg create --progress --stats "$XDG_RUNTIME_DIR/gvfs/smb-share:server=fileserver,share=files/misha-backup/borg::Dropbox$(date +%Y-%m-%dT%H-%M-%S)" "$HOME/Dropbox"
+exec borg create --progress --stats "$XDG_RUNTIME_DIR/gvfs/smb-share:server=fileserver,share=files,user=mixa/misha-backup/borg::Dropbox$(date +%Y-%m-%dT%H-%M-%S)" "$HOME/Dropbox"
